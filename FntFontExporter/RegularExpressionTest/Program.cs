61ed0522c68f8fee4c299756a1f0888db2152bd4
Vinh Nhon <thevinh92@gmail.com>
2015-07-09 11:17:18 +0700

    Initialize Solutions & Projects

---
+++ FntFontExporter/RegularExpressionTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionTest
+{
+    class Program
+    {
+        private static readonly string regexPattern = @"^\w+(\s|$)|\w+\=(\d+(\s|$)|\d+\,\d+(\s|$)|""[^""]*""|\d+\,\d+\,\d+\,\d+)";
+        private static readonly string content = "info font=\"Roboto Condensed\" size=64 bold=0 italic=0 charset=\"\" unicode=0 stretchH=100 smooth=1 aa=1 padding=2,2,2,2 spacing=2,2";
+
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(regexPattern);
+            MatchCollection matches = regex.Matches(content);
+            foreach (Match match in matches)
+            {
+                if (!match.Success)
+                    continue;
+
+                Console.WriteLine(match.Value);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
